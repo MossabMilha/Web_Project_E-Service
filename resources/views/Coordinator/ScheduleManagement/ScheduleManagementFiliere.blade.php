73ac38e5cfa2a6bb1e96f8a86b712348dd50e79e
MMILHA <mossabmilha.m@gmail.com>
2025-04-13 15:44:38 +0100

    Working In Coordinator : -Working In Schedule Management --->Some Advancement

--- resources/views/Coordinator/ScheduleManagement/ScheduleManagementFiliere.blade.php
+++ resources/views/Coordinator/ScheduleManagement/ScheduleManagementFiliere.blade.php
@@ -8,6 +8,30 @@
 </head>
 <body>
     <h1>📅 Schedule Management - {{$filiere->name}}</h1>
+    <table>
+        <tr>
+            <th>Hours</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+        </tr>
+        <tr>
+            <th>08:30 - 10:30</th>
+        </tr>
+        <tr>
+            <th>10:30 - 12:30</th>
+        </tr>
+        <tr>
+            <th>14:30 - 16:30</th>
+        </tr>
+        <tr>
+            <th>16:30 - 18:30</th>
+        </tr>
+
+    </table>
 
 </body>
 </html>
