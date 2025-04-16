64defe38ef03bd6eed3c3b11fe48658d24c7a903
Elafifiyoussef <elafifi.yo@gmail.com>
2025-04-16 21:38:07 +0100

    - adding coordinator cards image

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,6 @@
 <div class="card">
     <!-- Card Image (optional) -->
-{{--    <img src="" alt="Image Description" class="card-img-top">--}}
+    <img src="{{asset($cardImg)}}" alt="Image Description" class="card-img-top">
     {{-- TODO: remember to add an img--}}
 
     <!-- Card Header (optional) -->
@@ -11,6 +11,6 @@
     <!-- Card Body -->
     <div class="card-body">
         <p class="card-text">{{$slot}}</p>
-        <a href="{{$cardLink}}" class="btn btn-primary">manage</a>
+        <a href="{{route($cardLink)}}" class="btn btn-primary">manage</a>
     </div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,45 +10,56 @@
         ])
     </x-slot:head>
 
-{{--    here to include nav bar --}}
+    {{--    nav bar --}}
     <x-nav/>
 
-{{--    here to include content --}}
     {{--    header --}}
     @if(auth()->user())
-        <x-header :userrole="auth()->user()->role" :username="auth()->user()->name" :background_url="'png/3566.jpg'"/>
+        <x-header :userrole="auth()->user()->role" :username="auth()->user()->name" :background_url="'png/header-banner.jpg'"/>
     @endif
+
     {{--    main --}}
     <main class="p-2">
         <section class="tools-section">
             {{--    TODO: make h2 and p dynamic depending on the user--}}
-            <h2 class=" m-4 text-2xl font-semibold mb-2 text-gray-800">Academic Management Tools</h2>
-            <p class=" m-4 text-gray-600 mb-6">
+            <h2 class=" m-2 text-2xl font-semibold mb-2 text-gray-800">Academic Management Tools</h2>
+            <p class=" m-2 text-gray-600 mb-6">
                 Manage key academic operations including teaching units, contractual staff, and schedule planning.
             </p>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <div class="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-2">
             @if(auth()->user()->role == "admin")
-                <x-card :card_title="'E-Core Users'" :card_link="route('UserManagement.search')">
+                <x-card :card_img="'png/planing.jpg'" :card_title="'E-Core Users'" :card_link="'UserManagement.search'">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, voluptates, voluptatibus.
                 </x-card>
                 {{-- I Add This --}}{{--(X_X)--}}
-                <x-card :card_title="'E-Core Logs'" :card_link="route('logs.sort')">
+                <x-card :card_img="'png/schedule.jpg'" :card_title="'E-Core Logs'" :card_link="'logs.sort'">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, voluptates, voluptatibus.
                 </x-card>
             @elseif(auth()->user()->role == "department_head")
-                <x-card :card_title="'E-core Professors'" :card_link="route('department-head.professors.index')">
+                <x-card :card_img="'png/schedule.jpg'" :card_title="'E-core Professors'" :card_link="'department-head.professors.index'">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium, voluptates, voluptatibus.
                 </x-card>
             @elseif(auth()->user()->role == "coordinator")
-                <x-card :card_title="'Teaching Unit Management'" :card_link="route('Coordinator.teachingUnits')">
-                    Teaching Unit Management helps coordinators handle course content, assign instructors, and keep everything on schedule in one place.
-                </x-card>
-                <x-card :card_title="'Vacatire Management'" :card_link="route('VacataireAccount')">
+                <div class="flex justify-center">
+                    <x-card :card_img="'png/units.jpg'" :card_title="'Teaching Unit Management'" :card_link="'Coordinator.teachingUnits'">
+                        Teaching Unit Management helps coordinators handle course content, assign instructors, and keep everything on schedule in one place.
+                    </x-card>
+                </div>
+                <div class="flex justify-center">
+                <x-card :card_img="'png/part-time.jpg'" :card_title="'Vacatire Management'" :card_link="'VacataireAccount'">
                     Manage part-time instructors, their assignments, and teaching hours with ease.
                 </x-card>
-                <x-card :card_title="'Schedule Management'" :card_link="route('Coordinator.ScheduleManagement')">
+                </div>
+                <div class="flex justify-center">
+                <x-card :card_img="'png/schedule.jpg'" :card_title="'Schedule Management'" :card_link="'Coordinator.ScheduleManagement'">
                     Easily plan and manage course schedules, classrooms, and instructor availability.
                 </x-card>
+                </div>
+                <div class="flex justify-center">
+                <x-card :card_img="'png/dead.jpg'" :card_title="'he is dead'" :card_link="'Coordinator.ScheduleManagement'">
+                    Don't revive him X_X
+                </x-card>
+                </div>
             @endif
             </div>
         </section>
