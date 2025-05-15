d1fdd36c1cd27566c41b03ae8666be69bf584f04
Phos <elafifi.yo@gmail.com>
2025-05-15 02:11:27 +0100

    Improve filière selection UI in Schedule Management page

--- resources/views/Coordinator/ScheduleManagement/ScheduleManagement.blade.php
+++ resources/views/Coordinator/ScheduleManagement/ScheduleManagement.blade.php
@@ -11,19 +11,12 @@
     <div class="main-container">
         <h1 class="text-xl font-bold mb-6">Please Select One Of Your Filière</h1>
         <div class="form-group mb-8">
-{{--            <select id="filiereSelect" name="filiere_id" required class="form-select">--}}
-{{--                <option value="" disabled selected>Select a filière</option>--}}
-{{--                @foreach($filieres as $filiere)--}}
-{{--                    <option value="{{ $filiere->id }}" data-name="{{ $filiere->name }}">{{ $filiere->name }}</option>--}}
-{{--                @endforeach--}}
-{{--            </select>--}}
-            <h1>Select a filière: </h1>
             <form method="GET" id="form" action="">
                 @csrf
                 <div id="filiereSelect" class="filiere-select-container">
                     @foreach($filieres as $filiere)
                         <div class="filiere-option" data-value="{{ $filiere->id }}" data-name="{{ $filiere->name }}">
-                            <img src="{{asset('png/filieres/'.explode(' ', $filiere->name)[0].".jpg")}}" alt="{{$filiere->name}}" width="128px">
+                            <img src="{{asset('png/filieres/'.explode(' ', $filiere->name)[0].".jpg")}}" alt="{{$filiere->name}}">
                             <span class="option-label"> {{$filiere->name}}</span>
                         </div>
                     @endforeach
